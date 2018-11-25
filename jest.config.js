b3758612f2757550eed63f1e39cb5d616991c981
Marvin Hagemeister <marvin@marvinhagemeister.de>
2018-11-25 02:50:53 +0100

    Upgrade dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  globals: {
-    "ts-jest": {
-      enableTsDiagnostics: true,
-    },
-  },
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  testRegex: "(test|spec)\\.(jsx?|tsx?)$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  preset: "ts-jest",
 };
